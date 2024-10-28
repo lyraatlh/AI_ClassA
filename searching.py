d69f08e10876404199ee450b96fa86ad1701fa43
Lyra Attallah <153347678+lyraatlh@users.noreply.github.com>
2024-10-28 11:13:04 +0800

    add searching.py

---
+++ searching.py
@@ -0,0 +1,26 @@
+# buat program menggunakan metode searching
+# di berikan input
+# x = [5, 5, 10, 3, 2, 6, 7]
+# y1 = 4
+# y2 = 2
+# output : indeks jika y1 ada dalam list atau "0" jika y2 tidak ada dalam list
+
+def mencari_nilai(x, y1, y2):
+    # Mencari indeks y1 dalam daftar x
+    if y1 in x:
+        index_y1 = x.index(y1)
+    else:
+        index_y1 = 0
+
+    # Memeriksa keberadaan y2 dalam daftar x
+    if y2 not in x:
+        return 0
+    else:
+        return index_y1
+
+x = [5, 5, 10, 3, 2, 6, 7]
+y1 = 4
+y2 = 2
+
+hasil = mencari_nilai(x, y1, y2)
+print("Hasil", hasil)
